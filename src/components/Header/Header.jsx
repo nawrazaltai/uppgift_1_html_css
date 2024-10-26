5bfc61442f5a0b3ba4d7b9d7ee2e6bcd6c29bb4f
nawrazaltai <nawrasaltai4@gmail.com>
2024-10-26 10:43:32 +0200

    theme changer complete for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme == "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <header>
+      <div className="header_left_content">
+        <img src="assets/Link.png" alt="Silicon Logo" />
+
+        <a href="/" className="features_link">
+          Features
+        </a>
+      </div>
+
+      <div className="header_right_content">
+        <div className="theme_content_container" onClick={toggleTheme}>
+          <p className="dark_mode_title">{theme} mode</p>
+
+          <div className="switch_bg">
+            <span className="switch_inner_circle"></span>
+          </div>
+        </div>
+
+        <nav>
+          <span className="ham_1"></span>
+          <span className="ham_2"></span>
+          <span className="ham_3"></span>
+        </nav>
+
+        <button className="sign_in_btn">Sign in</button>
+      </div>
+    </header>
+  );
 };
